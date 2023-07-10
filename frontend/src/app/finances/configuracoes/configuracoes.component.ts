e37e4d5f6b0670040eb6a3c23e77fb3f3c4beed3
Tim Beck <timbeck1997@hotmail.com>
2023-07-10 00:02:05 -0300

    encerramento mensal

--- frontend/src/app/finances/configuracoes/configuracoes.component.ts
+++ frontend/src/app/finances/configuracoes/configuracoes.component.ts
@@ -6,6 +6,7 @@ import { take } from 'rxjs';
 import { User } from 'src/app/authentication/model/User';
 import { URL } from 'src/app/util/environment';
 import { UserComplete } from 'src/app/authentication/model/UserComplete';
+import { Util } from 'src/app/util/util';
 
 @Component({
   selector: 'app-configuracoes',
@@ -16,10 +17,18 @@ export class ConfiguracoesComponent {
 
   formulario: FormGroup;
   formularioAlterarSenha: FormGroup;
-  user:UserComplete;
-  alterarSenha:boolean=false;
+  user: UserComplete;
+  alterarSenha: boolean = false;
 
-  constructor(private auth:AuthenticationService,  private http: HttpClient){
+  meses: any = Util.getMeses();
+  anos: any = []
+  ano:string;
+  mes:string;
+
+  gastosFixos:boolean=true;
+  cartaoCredito:boolean=true;
+
+  constructor(private auth: AuthenticationService, private http: HttpClient) {
     this.formulario = new FormGroup({
       id: new FormControl('', Validators.required),
       name: new FormControl('', Validators.required),
@@ -33,26 +42,33 @@ export class ConfiguracoesComponent {
       newPassword2: new FormControl('', Validators.required),
 
     })
-    
+    for (let i = 2020; i <= new Date().getFullYear(); i++) {
+      this.anos.push(String(i));
+    }
+    let mes = new Date().getMonth();
+    let mesString = mes < 10 ? '0' + mes : String(mes);
+    this.ano=String(new Date().getFullYear()),
+    this.mes=mesString
+
   }
-  ngOnInit(){
-    this.user=this.auth.getUser();
+  ngOnInit() {
+    this.user = this.auth.getUser();
     this.formulario.patchValue(this.user);
-    
+
   }
-  
-  salvarSenha(){
-    if(this.formularioAlterarSenha.valid){
-      if(this.formularioAlterarSenha.get('newPassword')?.value!==this.formularioAlterarSenha.get('newPassword2')?.value){
+
+  salvarSenha() {
+    if (this.formularioAlterarSenha.valid) {
+      if (this.formularioAlterarSenha.get('newPassword')?.value !== this.formularioAlterarSenha.get('newPassword2')?.value) {
         alert('As novas senhas devem ser iguais')
-      }else{
+      } else {
         this.auth.alterarSenha(this.formularioAlterarSenha.value)
       }
-    }else{
+    } else {
       alert('Preencha todos campos')
     }
   }
-  salvarUsuario(){
+  salvarUsuario() {
     const url = URL;
     this.http.put<UserComplete>(url + '/users', this.formulario.value)
       .pipe(take(1))
@@ -60,12 +76,15 @@ export class ConfiguracoesComponent {
         this.auth.setUser(result)
         alert('Dados alterados com sucesso')
       });
-  
+
   }
   verificaValidTouched(campo: string) {
     return !this.formulario.get(campo)?.valid && this.formulario.get(campo)?.touched;
   }
   verificaValidTouchedChangePassword(campo: string) {
     return !this.formularioAlterarSenha.get(campo)?.valid && this.formularioAlterarSenha.get(campo)?.touched;
+  }
+  handleChecked(value:any){
+    
   }
 }
